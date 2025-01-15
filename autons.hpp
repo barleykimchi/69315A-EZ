221942999a7c411651cff1984804b485590d983c
barleykimchi <kmhuang2007@gmail.com>
2025-01-15 16:01:24 -0600

    autons.hpp
    1/15/25

--- autons.hpp
+++ autons.hpp
@@ -1,49 +1,29 @@
 #pragma once
 
-#include "EZ-Template/drive/drive.hpp"
-#include "EZ-Template/piston.hpp"
-#include "pros/adi.hpp"
-
-// Motors used in autons
-extern Drive chassis;
-extern pros::v5::MotorGroup intake;
-extern pros::v5::Motor ladybrown_Left;
-extern pros::v5::Motor ladybrown_Right;
-
-// Pistons used in autons
-extern pros::adi::Pneumatics mogoL;
-extern pros::adi::Pneumatics mogoR;
-extern pros::adi::Pneumatics colorSort;
-extern ez::Piston doinker;
-extern ez::Piston hang;
-
-// Sensors used in autons
-extern pros::v5::Imu imu;
-extern pros::v5::Optical optical;
-
-// Functions used in autons
-void extendMogo();
-void retractMogo();
 void default_constants();
 
-// Autons used in matches
-void br_NOWS();
-void rr_NOWS();
-void bl_NOWS();
-void rl_NOWS();
-void EMERGENCY_AUTON();
-void color_sort_test();
-void spitRED_task();
-void spitBLUE_task();
+void drive_example();
+void turn_example();
+void drive_and_turn();
+void wait_until_change_speed();
+void swing_example();
+void motion_chaining();
+void combining_movements();
+void interfered_example();
+void odom_drive_example();
+void odom_pure_pursuit_example();
+void odom_pure_pursuit_wait_until_example();
+void odom_boomerang_example();
+void odom_boomerang_injected_pure_pursuit_example();
+void measure_offsets();
+
+void blueSAWP();
+void redSAWP();
 void skills();
 
-void blue_stake();
-void red_stake();
+extern pros::adi::Pneumatics mogoL;
+extern pros::adi::Pneumatics mogoR;
+extern pros::Motor intake;
 
-// Example autons from EZ Template
-// void motion_chaining();
-// void combining_movements();
-// void interfered_example();
-
-void newAWP_RED();
-void newAWP_BLUE();
+void extendMogo();
+void retractMogo();
