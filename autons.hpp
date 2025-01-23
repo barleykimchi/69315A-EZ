3a33a739c4dad04fb733db079fec1f53b9a21cee
barleykimchi <kmhuang2007@gmail.com>
2025-01-23 11:08:47 -0600

    autons.hpp
    1/23/2025

--- autons.hpp
+++ autons.hpp
@@ -2,28 +2,20 @@
 
 void default_constants();
 
-void drive_example();
-void turn_example();
-void drive_and_turn();
-void wait_until_change_speed();
-void swing_example();
-void motion_chaining();
-void combining_movements();
-void interfered_example();
-void odom_drive_example();
-void odom_pure_pursuit_example();
-void odom_pure_pursuit_wait_until_example();
-void odom_boomerang_example();
-void odom_boomerang_injected_pure_pursuit_example();
-void measure_offsets();
-
 void blueSAWP();
 void redSAWP();
 void skills();
+void turnTest();
+void colorSort();
 
 extern pros::adi::Pneumatics mogoL;
 extern pros::adi::Pneumatics mogoR;
+
 extern pros::Motor intake;
+extern pros::Motor ladybrown_Left;
+extern pros::Motor ladybrown_Right;
+
+extern pros::v5::Optical optical;
 
 void extendMogo();
 void retractMogo();
