aa123472debfa4f9306c8e875510122e24272bff
barleykimchi <kmhuang2007@gmail.com>
2025-01-25 10:57:36 -0600

    Update autons.cpp
    1/25/2025

--- autons.cpp
+++ autons.cpp
@@ -10,24 +10,19 @@ int DRIVE_SPEED = 70;
 const int TURN_SPEED = 90;
 const int SWING_SPEED = 110;
 
-///
 // Constants
-///
 void default_constants() {
   // P, I, D, and Start I
-  chassis.pid_drive_constants_set(20.0, 0.0, 100.0);         // Fwd/rev constants, used for odom and non odom motions
-  chassis.pid_heading_constants_set(11.0, 0.0, 20.0);        // Holds the robot straight while going forward without odom
+  chassis.pid_drive_constants_set(20.0, 0.0, 100.0);         // Fwd/rev constants
+  chassis.pid_heading_constants_set(11.0, 0.0, 20.0);        // Holds the robot straight while going forward
   chassis.pid_turn_constants_set(3.0, 0.05, 20.0, 15.0);     // Turn in place constants
   chassis.pid_swing_constants_set(6.0, 0.0, 65.0);           // Swing constants
-  chassis.pid_odom_angular_constants_set(6.5, 0.0, 52.5);    // Angular control for odom motions
-  chassis.pid_odom_boomerang_constants_set(5.8, 0.0, 32.5);  // Angular control for boomerang motions
 
   // Exit conditions
   chassis.pid_turn_exit_condition_set(90_ms, 3_deg, 250_ms, 7_deg, 500_ms, 500_ms);
   chassis.pid_swing_exit_condition_set(90_ms, 3_deg, 250_ms, 7_deg, 500_ms, 500_ms);
   chassis.pid_drive_exit_condition_set(90_ms, 1_in, 250_ms, 3_in, 500_ms, 500_ms);
-  chassis.pid_odom_turn_exit_condition_set(90_ms, 3_deg, 250_ms, 7_deg, 500_ms, 750_ms);
-  chassis.pid_odom_drive_exit_condition_set(90_ms, 1_in, 250_ms, 3_in, 500_ms, 750_ms);
+
   chassis.pid_turn_chain_constant_set(3_deg);
   chassis.pid_swing_chain_constant_set(5_deg);
   chassis.pid_drive_chain_constant_set(3_in);
@@ -37,42 +32,12 @@ void default_constants() {
   chassis.slew_drive_constants_set(3_in, 70);
   chassis.slew_swing_constants_set(3_in, 80);
 
-  // The amount that turns are prioritized over driving in odom motions
-  // - if you have tracking wheels, you can run this higher.  1.0 is the max
-  chassis.odom_turn_bias_set(0.9);
-
-  chassis.odom_look_ahead_set(7_in);           // This is how far ahead in the path the robot looks at
-  chassis.odom_boomerang_distance_set(16_in);  // This sets the maximum distance away from target that the carrot point can be
-  chassis.odom_boomerang_dlead_set(0.625);     // This handles how aggressive the end of boomerang motions are
-
-  chassis.pid_angle_behavior_set(ez::shortest);  // Changes the default behavior for turning, this defaults it to the shortest path there
+  chassis.pid_angle_behavior_set(ez::shortest);  // Changes the default behavior to the shortest path 
 }
 
 void turnTest(){
-  chassis.pid_turn_set(90_deg, TURN_SPEED);
+  chassis.pid_turn_set(180, TURN_SPEED, ez::clockwise);
   chassis.pid_wait();
-}
-
-void colorSort(){
-
-  // bool intakeStatus = true; 
-
-  while(true){
-    double hue = optical.get_hue();
-
-    if(hue > 200 && hue < 360){
-      intake.move(127);
-      pros::delay(100);
-
-      intake.move(0);
-      pros::delay(400);
-
-      intake.move(127);
-    } else {
-      intake.move(0);
-    }
-      pros::delay(10);
-  }  
 }
 
 void blueSAWP(){ // Blue SAWP
@@ -139,7 +104,7 @@ void blueSAWP(){ // Blue SAWP
   // Drive to and collect third ring
   chassis.pid_drive_set(14_in, 40);
   chassis.pid_wait();
-  pros::delay(300);
+  pros::delay(500);
 
   // Back up
   chassis.pid_drive_set(-10_in, DRIVE_SPEED);
@@ -161,7 +126,7 @@ void redSAWP(){ // Red SAWP
   DRIVE_SPEED = 110; 
 
   // Drive and turn to alliance stake
-  chassis.pid_drive_set(-5_in, DRIVE_SPEED);
+  chassis.pid_drive_set(-7_in, DRIVE_SPEED);
   chassis.pid_wait_quick_chain();
 
   chassis.pid_turn_set(90_deg, TURN_SPEED);
@@ -180,19 +145,17 @@ void redSAWP(){ // Red SAWP
   chassis.pid_wait_quick_chain();
 
   // Turn to mogo
-  chassis.pid_turn_set(-125_deg, TURN_SPEED);
+  chassis.pid_turn_set(-130_deg, TURN_SPEED);
   chassis.pid_wait();
 
   // Drive to and hold mogo
-  chassis.pid_drive_set(-40_in, DRIVE_SPEED);
+  extendMogo();
+  chassis.pid_drive_set(-38_in, DRIVE_SPEED);
   chassis.pid_wait_until(-13_in);
   chassis.pid_speed_max_set(60);
   chassis.pid_wait();
   pros::delay(200);
   retractMogo();
-
-  chassis.pid_drive_set(1_in, DRIVE_SPEED);
-  chassis.pid_wait_quick_chain();
 
   // Turn to 1-stack
   chassis.pid_turn_set(0_deg, TURN_SPEED);
@@ -204,7 +167,7 @@ void redSAWP(){ // Red SAWP
   chassis.pid_wait();
   
   // Turn to 4-stack
-  chassis.pid_turn_set(91_deg, TURN_SPEED);
+  chassis.pid_turn_set(95_deg, TURN_SPEED);
   chassis.pid_wait_until(90_deg);
 
   // Drive to and collect second ring
@@ -217,27 +180,36 @@ void redSAWP(){ // Red SAWP
   chassis.pid_wait_quick_chain();
 
   // Turn to third ring
-  chassis.pid_turn_set(60_deg, TURN_SPEED);
-  chassis.pid_wait_until(55_deg);
+  chassis.pid_turn_set(65_deg, TURN_SPEED);
+  chassis.pid_wait_until(60_deg);
 
   // Drive to and collect third ring
   chassis.pid_drive_set(14_in, 40);
   chassis.pid_wait();
-  pros::delay(300);
+  pros::delay(500);
 
   // Back up
   chassis.pid_drive_set(-10_in, DRIVE_SPEED);
   chassis.pid_wait_quick_chain();
 
   // Turn towards ladder
-  chassis.pid_turn_set(182_deg, TURN_SPEED);
-  chassis.pid_wait_until(180_deg);
+  chassis.pid_turn_set(162_deg, TURN_SPEED);
+  chassis.pid_wait_until(160_deg);
+  intake.move(0);
 
   // Drive to and touch ladder
   chassis.pid_drive_set(34_in, DRIVE_SPEED);
   chassis.pid_wait_until(20_in);
   chassis.pid_speed_max_set(30);
   chassis.pid_wait();
+
+  ladybrown_Left.move(-127);
+  ladybrown_Right.move(127);
+
+  pros::delay(500);
+
+  ladybrown_Left.move(0);
+  ladybrown_Right.move(0);
 }
 
 void skills(){
@@ -261,43 +233,155 @@ void skills(){
   retractMogo();
   pros::delay(500);
 
-  // Turn to first ring
+  // Turn to 1st ring
   chassis.pid_turn_set(-10_deg, TURN_SPEED);
   chassis.pid_wait();
 
-  // Drive to and collect first ring
+  // Drive to and collect 1st ring
   intake.move(127);
-  chassis.pid_drive_set(18_in, DRIVE_SPEED);
-  chassis.pid_wait_quick_chain();
-
-  // Turn to second ring
-  pros::delay(500);
-  chassis.pid_turn_set(90_deg, TURN_SPEED);
-  chassis.pid_wait_quick_chain();
-
-  // Drive to and collect second ring
   chassis.pid_drive_set(20_in, DRIVE_SPEED);
   chassis.pid_wait_quick_chain();
 
-  // Turn to third ring
-  pros::delay(500);
+  // Turn to 2nd ring
+  chassis.pid_turn_set(90_deg, TURN_SPEED);
+  chassis.pid_wait_quick_chain();
+
+  // Drive to and collect 2nd ring
+  chassis.pid_drive_set(34_in, DRIVE_SPEED);
+  chassis.pid_wait_quick_chain();
+
+  // Turn to 3rd ring
+  chassis.pid_turn_set(0_deg, TURN_SPEED);
+  chassis.pid_wait_quick_chain();
+
+  // Drive to and collect 3rd ring
+  chassis.pid_drive_set(25_in, DRIVE_SPEED);
+  chassis.pid_wait_quick_chain();
+
+  // Turn to 4th ring
   chassis.pid_turn_set(180_deg, TURN_SPEED);
   chassis.pid_wait_quick_chain();
   
-  // Drive to and collect third ring
+  // Drive to and collect 4th ring
+  chassis.pid_drive_set(50_in, DRIVE_SPEED);
+  chassis.pid_wait();
+
+  // Back up
+  chassis.pid_drive_set(-15_in, DRIVE_SPEED);
+  chassis.pid_wait();
+
+  // Turn to 5th & 6th rings
+  chassis.pid_turn_set(-90_deg, DRIVE_SPEED);
+  chassis.pid_wait_quick_chain();
+
+  chassis.pid_drive_set(5_in, DRIVE_SPEED);
+  chassis.pid_wait_quick_chain();
+
+  chassis.pid_turn_set(-180_deg, DRIVE_SPEED);
+  chassis.pid_wait_quick_chain();
+
+  // Drive to and collect 5th and 6th rings
+  chassis.pid_drive_set(30_in, DRIVE_SPEED);
+  chassis.pid_wait();
+
+  // Score 5th & 6th rings
+  pros::delay(1500);
+
+  // Turn to corner
+  chassis.pid_turn_set(295_deg, TURN_SPEED);
+  chassis.pid_wait_quick_chain();
+
+  // Drive to corner and deposit mogo
+  chassis.pid_drive_set(-12_in, DRIVE_SPEED);
+  chassis.pid_wait_quick_chain();
+  extendMogo();
+
+  // Drive towards center
+  chassis.pid_drive_set(25_in, DRIVE_SPEED);
+  chassis.pid_wait_quick_chain();
+
+  // Turn towards 2nd mogo
+  chassis.pid_turn_set(-270_deg, TURN_SPEED);
+  chassis.pid_wait();
+
+  // Other side of field:
+  // Drive to and hold 2nd mogo
+  chassis.pid_drive_set(-55_in, 60);
+  chassis.pid_wait_until(-50_in);
+  retractMogo();
+  pros::delay(500);
+
+  // Back up
+  chassis.pid_drive_set(1_in, DRIVE_SPEED);
+  chassis.pid_wait_quick_chain();
+
+  // Turn to 1st ring
+  chassis.pid_turn_set(0_deg, TURN_SPEED);
+  chassis.pid_wait_quick_chain();
+
+  // Drive to and collect 1st ring
+  chassis.pid_drive_set(20_in, DRIVE_SPEED);
+  chassis.pid_wait();
+  pros::delay(500);
+
+   // Turn to 2nd ring
+  chassis.pid_turn_set(-90_deg, TURN_SPEED);
+  chassis.pid_wait_quick_chain();
+
+  // Drive to and collect 2nd ring
+  chassis.pid_drive_set(36_in, DRIVE_SPEED);
+  chassis.pid_wait_quick_chain();
+
+  // Turn to 3rd ring
+  chassis.pid_turn_set(-360_deg, TURN_SPEED);
+  chassis.pid_wait_quick_chain();
+
+  // Drive to and collect 3rd ring
+  chassis.pid_drive_set(25_in, DRIVE_SPEED);
+  chassis.pid_wait_quick_chain();
+
+  // Turn to 4th ring
+  chassis.pid_turn_set(180_deg, TURN_SPEED, ez::counterclockwise);
+  chassis.pid_wait_quick_chain();
+  
+  // Drive to and collect 4th ring
+  chassis.pid_drive_set(50_in, DRIVE_SPEED);
+  chassis.pid_wait();
+
+  // Back up
+  chassis.pid_drive_set(-15_in, DRIVE_SPEED);
+  chassis.pid_wait();
+
+  // Turn to 5th and 6th rings
+  chassis.pid_turn_set(90_deg, DRIVE_SPEED);
+  chassis.pid_wait_quick_chain();
+
+  chassis.pid_drive_set(5_in, DRIVE_SPEED);
+  chassis.pid_wait_quick_chain();
+
+  chassis.pid_turn_set(180_deg, DRIVE_SPEED);
+  chassis.pid_wait_quick_chain();
+
+  // Drive to and collect 5th and 6th rings
   chassis.pid_drive_set(25_in, DRIVE_SPEED);
   chassis.pid_wait();
-  pros::delay(1000);
 
-  // Back up and turn to fourth ring
-  chassis.pid_drive_set(-10_in, DRIVE_SPEED);
+  // Wait for rings to score
+  pros::delay(1500);
+
+  // Turn to corner
+  chassis.pid_turn_set(60_deg, TURN_SPEED);                 // TEST TEST TEST!
+  chassis.pid_wait_quick_chain();                             // DOWNLOAD DOWNLOAD DOWNLOAD!
+
+  // Deposit mogo in corner
+  chassis.pid_drive_set(-14_in, DRIVE_SPEED);
   chassis.pid_wait_quick_chain();
-  chassis.pid_turn_set(-150_deg, TURN_SPEED);
+  extendMogo();
+
+  pros::delay(500);
+
+  chassis.pid_drive_set(10_in, DRIVE_SPEED);
   chassis.pid_wait();
-
-  // Drive to and collect fourth ring
-
-
 }
 
 ///
@@ -486,141 +570,4 @@ void interfered_example() {
 
   chassis.pid_turn_set(90_deg, TURN_SPEED);
   chassis.pid_wait();
-}
-
-///
-// Odom Drive PID
-///
-void odom_drive_example() {
-  // This works the same as pid_drive_set, but it uses odom instead!
-  // You can replace pid_drive_set with pid_odom_set and your robot will
-  // have better error correction.
-
-  chassis.pid_odom_set(24_in, DRIVE_SPEED, true);
-  chassis.pid_wait();
-
-  chassis.pid_odom_set(-12_in, DRIVE_SPEED);
-  chassis.pid_wait();
-
-  chassis.pid_odom_set(-12_in, DRIVE_SPEED);
-  chassis.pid_wait();
-}
-
-///
-// Odom Pure Pursuit
-///
-void odom_pure_pursuit_example() {
-  // Drive to 0, 30 and pass through 6, 10 and 0, 20 on the way, with slew
-  chassis.pid_odom_set({{{6_in, 10_in}, fwd, DRIVE_SPEED},
-                        {{0_in, 20_in}, fwd, DRIVE_SPEED},
-                        {{0_in, 30_in}, fwd, DRIVE_SPEED}},
-                       true);
-  chassis.pid_wait();
-
-  // Drive to 0, 0 backwards
-  chassis.pid_odom_set({{0_in, 0_in}, rev, DRIVE_SPEED},
-                       true);
-  chassis.pid_wait();
-}
-
-///
-// Odom Pure Pursuit Wait Until
-///
-void odom_pure_pursuit_wait_until_example() {
-  chassis.pid_odom_set({{{0_in, 24_in}, fwd, DRIVE_SPEED},
-                        {{12_in, 24_in}, fwd, DRIVE_SPEED},
-                        {{24_in, 24_in}, fwd, DRIVE_SPEED}},
-                       true);
-  chassis.pid_wait_until_index(1);  // Waits until the robot passes 12, 24
-  // Intake.move(127);  // Set your intake to start moving once it passes through the second point in the index
-  chassis.pid_wait();
-  // Intake.move(0);  // Turn the intake off
-}
-
-///
-// Odom Boomerang
-///
-void odom_boomerang_example() {
-  chassis.pid_odom_set({{0_in, 24_in, 45_deg}, fwd, DRIVE_SPEED},
-                       true);
-  chassis.pid_wait();
-
-  chassis.pid_odom_set({{0_in, 0_in, 0_deg}, rev, DRIVE_SPEED},
-                       true);
-  chassis.pid_wait();
-}
-
-///
-// Odom Boomerang Injected Pure Pursuit
-///
-void odom_boomerang_injected_pure_pursuit_example() {
-  chassis.pid_odom_set({{{0_in, 24_in, 45_deg}, fwd, DRIVE_SPEED},
-                        {{12_in, 24_in}, fwd, DRIVE_SPEED},
-                        {{24_in, 24_in}, fwd, DRIVE_SPEED}},
-                       true);
-  chassis.pid_wait();
-
-  chassis.pid_odom_set({{0_in, 0_in, 0_deg}, rev, DRIVE_SPEED},
-                       true);
-  chassis.pid_wait();
-}
-
-///
-// Calculate the offsets of your tracking wheels
-///
-void measure_offsets() {
-  // Number of times to test
-  int iterations = 10;
-
-  // Our final offsets
-  double l_offset = 0.0, r_offset = 0.0, b_offset = 0.0, f_offset = 0.0;
-
-  // Reset all trackers if they exist
-  if (chassis.odom_tracker_left != nullptr) chassis.odom_tracker_left->reset();
-  if (chassis.odom_tracker_right != nullptr) chassis.odom_tracker_right->reset();
-  if (chassis.odom_tracker_back != nullptr) chassis.odom_tracker_back->reset();
-  if (chassis.odom_tracker_front != nullptr) chassis.odom_tracker_front->reset();
-  
-  for (int i = 0; i < iterations; i++) {
-    // Reset pid targets and get ready for running an auton
-    chassis.pid_targets_reset();
-    chassis.drive_imu_reset();
-    chassis.drive_sensor_reset();
-    chassis.drive_brake_set(MOTOR_BRAKE_HOLD);
-    chassis.odom_xyt_set(0_in, 0_in, 0_deg);
-    double imu_start = chassis.odom_theta_get();
-    double target = i % 2 == 0 ? 90 : 270;  // Switch the turn target every run from 270 to 90
-
-    // Turn to target at half power
-    chassis.pid_turn_set(target, 63, ez::raw);
-    chassis.pid_wait();
-    pros::delay(250);
-
-    // Calculate delta in angle
-    double t_delta = util::to_rad(fabs(util::wrap_angle(chassis.odom_theta_get() - imu_start)));
-
-    // Calculate delta in sensor values that exist
-    double l_delta = chassis.odom_tracker_left != nullptr ? chassis.odom_tracker_left->get() : 0.0;
-    double r_delta = chassis.odom_tracker_right != nullptr ? chassis.odom_tracker_right->get() : 0.0;
-    double b_delta = chassis.odom_tracker_back != nullptr ? chassis.odom_tracker_back->get() : 0.0;
-    double f_delta = chassis.odom_tracker_front != nullptr ? chassis.odom_tracker_front->get() : 0.0;
-
-    // Calculate the radius that the robot traveled
-    l_offset += l_delta / t_delta;
-    r_offset += r_delta / t_delta;
-    b_offset += b_delta / t_delta;
-    f_offset += f_delta / t_delta;
-  }
-
-  // Average all offsets
-  l_offset /= iterations;
-  r_offset /= iterations;
-  b_offset /= iterations;
-  f_offset /= iterations;
-
-  // Set new offsets to trackers that exist
-  if (chassis.odom_tracker_left != nullptr) chassis.odom_tracker_left->distance_to_center_set(l_offset);
-  if (chassis.odom_tracker_right != nullptr) chassis.odom_tracker_right->distance_to_center_set(r_offset);
-  if (chassis.odom_tracker_back != nullptr) chassis.odom_tracker_back->distance_to_center_set(b_offset);
-  if (chassis.odom_tracker_front != nullptr) chassis.odom_tracker_front->distance_to_center_set(f_offset);
 }
