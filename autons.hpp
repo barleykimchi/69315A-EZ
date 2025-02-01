63fa0c0ec7ad605c8e0d9b8c398acdca25c602fb
barleykimchi <kmhuang2007@gmail.com>
2025-01-31 21:43:34 -0600

    autons.hpp
    1/31/2025

--- autons.hpp
+++ autons.hpp
@@ -2,19 +2,33 @@
 
 void default_constants();
 
-void blueSAWP();
-void redSAWP();
-void skills();
-void turnTest();
+// Color sorter variables declarations
+extern bool sorterEnabled;
+extern int targetHue;
 
+// Controller auton selector variable declarations
+extern int currAuton;
+extern const int NUM_AUTONS;
+extern string allAutons[]; 
+
+// Pneumatic declarations
 extern pros::adi::Pneumatics mogoL;
 extern pros::adi::Pneumatics mogoR;
 
+// Motor declarations
 extern pros::Motor intake;
 extern pros::Motor ladybrown_Left;
 extern pros::Motor ladybrown_Right;
 
+// Sensor declarations
 extern pros::v5::Optical optical;
 
+// Movement function declarations
 void extendMogo();
 void retractMogo();
+
+// Autonomous programs
+void blueSAWP();
+void redSAWP();
+void skills();
+void turnTest();
